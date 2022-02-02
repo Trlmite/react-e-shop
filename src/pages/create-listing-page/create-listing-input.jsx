aeadbca83ef0250acf05304335706e7f2fda0792
Trollmite <m.gytautas@gmail.com>
2022-02-02 18:00:08 +0200

    create-listing-input layout

--- src/pages/create-listing-page/create-listing-input.jsx
+++ src/pages/create-listing-page/create-listing-input.jsx
@@ -24,7 +24,15 @@ const CreateListingInput = () => {
   const pageName = 'createListingInput';
 
   return (
-    <Box>
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      textAlign: 'center',
+      width: 2 / 3,
+      pl: 2,
+    }}
+    >
       <Typography>{pageName}</Typography>
       <InputLabel htmlFor="title" sx={{ fontWeight: 600, my: 1 }}>Title</InputLabel>
       <TextField
@@ -41,42 +49,54 @@ const CreateListingInput = () => {
         sx={{ my: 1 }}
         id="description"
       />
-      <Box>
-        <InputLabel htmlFor="price" sx={{ fontWeight: 600, my: 1 }}>Price</InputLabel>
-        <TextField
-          sx={{ my: 1 }}
-          type="number"
-          id="price"
-        />
-        <InputLabel htmlFor="currency" sx={{ fontWeight: 600, my: 1 }}>Currency</InputLabel>
-        <TextField
-          sx={{ my: 1 }}
-          select
-          id="currency"
-        >
+      <Box sx={{
+        display: 'flex', alignItems: 'baseline', justifyContent: 'space-between', flexBasis: 'auto',
+      }}
+      >
+        <Box>
+          <InputLabel htmlFor="price" sx={{ fontWeight: 600 }}>Price</InputLabel>
+          <TextField
+            sx={{ my: 1 }}
+            type="number"
+            id="price"
+          />
+        </Box>
+        <Box>
+          <InputLabel htmlFor="currency" sx={{ fontWeight: 600 }}>Currency</InputLabel>
+          <TextField
+            sx={{ my: 1 }}
+            fullWidth
+            select
+            id="currency"
+          >
 
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
+            {currencies.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <InputLabel htmlFor="new" sx={{ fontWeight: 600, my: 1 }}>Condition</InputLabel>
+          <Box>
+
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              sx={{ mx: 2 }}
+              label="New"
+              id="new"
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Used"
+              id="used"
+            />
+          </Box>
+        </Box>
       </Box>
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <InputLabel htmlFor="new" sx={{ fontWeight: 600, mx: 1 }}>Condition</InputLabel>
-        <FormControlLabel
-          control={<Checkbox defaultChecked />}
-          label="New"
-          id="new"
-        />
-        <FormControlLabel
-          control={<Checkbox />}
-          label="Used"
-          id="used"
-        />
-      </Box>
-      <Box sx={{ display: 'flex', alignItems: 'center' }}>
-        <InputLabel htmlFor="manufactorer" sx={{ fontWeight: 600, mx: 1 }}>GPU&apos;s specs</InputLabel>
+        <InputLabel htmlFor="manufactorer" sx={{ fontWeight: 600, my: 1 }}>GPU&apos;s specs</InputLabel>
         <Box>
           <InputLabel htmlFor="manufacturer" sx={{ fontWeight: 600, my: 1 }}>Manufacturer</InputLabel>
           <TextField
--- src/pages/homepage/home-page.jsx
+++ src/pages/homepage/home-page.jsx
@@ -5,7 +5,7 @@ import MainButton from '../../components/button/main-button';
 import ItemCreationComponent from '../../components/item-create-component/item-create-component';
 
 const HomePage = () => (
-  <Box sx={{ mx: 2, my: 2 }}>
+  <Box sx={{ my: 2 }}>
     <Typography>
       Krabauskis Namu Puslapiauskiss
     </Typography>
